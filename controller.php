9953bf9e3769818a1636826e12b425da841316cf
mengguoru <2506839494@qq.com>
2016-03-08 17:59:54 +0800

    finished ajax to return json to front-end

--- controller.php
+++ controller.php
@@ -7,4 +7,17 @@
 require_once "autoload.php";
 
 use app\QueryIDCard;
-print_r(QueryIDCard::query("420984198704207896")) ;
+//以下为将要返回给前端页面的数据
+//$request = $_POST;
+//$request = $request['num_ID_card'];
+//$response = QueryIDCard::query($request);
+
+//先测试是否能正确返回数组类型数据
+$request= QueryIDCard::query('420984198704207896');
+$response = null;
+if($request['errNum']==0){
+    $response['code'] = 200;
+    $response['msg'] = $request['retData'];
+}
+//返回json 格式
+echo json_encode($response);
---
+++ static/js/queryIDCard.js
@@ -0,0 +1,39 @@
+/**
+ *  description :
+ *  dependance  : jquery
+ *  author      : Administrator
+ *  Date        : 2016/3/8 9:21
+ */
+var ID_QUERY ={};
+//封装jquery ajax方法
+ID_QUERY.ajax = function(url,params,callback){
+    $.ajax({
+        url:url,
+        type:'POST',
+        data:params,
+        dataType:'json',
+        success:callback,
+        error:function(){
+        alert('身份证输入错误');
+    }
+    });
+};
+/**
+ * callback函数的da应是json格式
+ */
+ID_QUERY.dataCallback= function(data){
+    if(data.code == 200){
+        var sex = null;
+        if(data.msg.sex == 'M')
+            sex = '男';
+        else
+            sex = '女';
+        $('#sex').text(sex);
+        $('#bithday').text(data.msg.birthday);
+        $('#address').text(data.msg.address);
+        $('#showResult').show();
+    }else{
+        alert('查询失败');
+        $('#showResult').hide();
+    }
+};
